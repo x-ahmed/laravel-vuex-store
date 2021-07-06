4df781bb38cbc1228b77594742edd58b150830a6
x-ahmed <appsxapp@gmail.com>
2021-07-06 13:38:08 +0200

    designed header component

---
+++ frontend/src/components/Dropdown.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul
+      class="dropdown-menu"
+      aria-labelledby="navbarScrollingDropdown"
+      @click.stop
+  >
+    <li>
+      <div class="dropdown-item">
+        <div class="d-flex justify-content-evenly align-items-baseline">
+          <div class="me-4">lorem lipsum test</div>
+          <div>
+            <span class="border-0 badge rounded-pill bg-warning text-black-50">5</span>
+            <span class="border-0 badge rounded-pill bg-success mx-1">$ 5</span>
+            <button class="border-0 badge rounded-pill bg-danger">X</button>
+          </div>
+        </div>
+      </div>
+    </li>
+    <li><hr class="dropdown-divider"></li>
+    <li>
+      <div class="dropdown-item">
+        <div class="d-flex justify-content-between align-items-baseline">
+          <div>Total</div>
+          <span class="border-0 badge bg-primary w-25">$ 500</span>
+        </div>
+      </div>
+      <div class="d-grid gap-2 mx-2 my-1">
+        <button
+            class="btn btn-danger btn-sm"
+            type="button"
+        >
+          Clear Cart
+        </button>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Dropdown",
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark pt-3">
+  <div class="container">
+    <a class="navbar-brand" href="#">Vuex Store</a>
+    <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarScroll"
+        aria-controls="navbarScroll"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul
+          class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll d-flex align-items-center"
+          style="--bs-scroll-height: 100px;"
+      >
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+              class="nav-link dropdown-toggle position-relative"
+              href="#"
+              id="navbarScrollingDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+          >
+              Cart
+              <span class="position-absolute top-0 start-200 translate-middle badge rounded-pill bg-info">
+                99+
+              </span>
+          </a>
+          <Dropdown />
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+import Dropdown from './Dropdown'
+
+export default {
+  name: "Header",
+  components: {
+    Dropdown,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
